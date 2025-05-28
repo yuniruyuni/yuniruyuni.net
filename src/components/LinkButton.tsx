4872e06a1f389022a5d546ed3666a74c7fb7d6ff
yuniruyuni <yuniruyuni@gmail.com>
2025-05-29 03:57:02 +0900

    refactor: split TwitterLinkGroup into sub-component

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -4,13 +4,17 @@ interface LinkButtonProps {
   href: string;
   children: React.ReactNode;
   className?: string;
+  baseClassName?: string;
 }
 
-export default function LinkButton({ href, children, className = '' }: LinkButtonProps) {
-  const baseClasses = "block w-full md:w-auto text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out";
-  
+export default function LinkButton({ 
+  href, 
+  children, 
+  className = '',
+  baseClassName = "block w-full md:w-auto bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+}: LinkButtonProps) {
   return (
-    <a href={href} className={`${baseClasses} ${className}`}>
+    <a href={href} className={`${baseClassName} ${className}`}>
       {children}
     </a>
   );
--- src/components/TwitterLinkGroup.tsx
+++ src/components/TwitterLinkGroup.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import LinkGroup from './LinkGroup';
 
 export default function TwitterLinkGroup() {
-  return (
-    <div className="w-full md:w-auto flex flex-row">
-      <a href="https://twitter.com/yuniruyuni" className="w-full bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-l-full border-r border border-dotted border-white transition duration-300 ease-in-out">
-        Twitter(X)
-      </a>
-      <a href="https://twitter.com/hashtag/yunicode" className="relative w-fill bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 border-r border border-dotted border-white transition duration-300 ease-in-out">
-        <span className="absolute top-0 left-1 text-xs">Tag</span>
-        <span className="text-sm">#yunicode</span>
-      </a>
-      <a href="https://twitter.com/hashtag/yunigraphics" className="relative flex-1 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-r-full transition duration-300 ease-in-out">
-        <span className="absolute top-0 left-1 text-xs">FanArt</span>
-        <span className="text-sm">#yunigraphics</span>
-      </a>
-    </div>
-  );
+  const links = [
+    {
+      href: "https://twitter.com/yuniruyuni",
+      text: "Twitter(X)"
+    },
+    {
+      href: "https://twitter.com/hashtag/yunicode", 
+      label: "Tag",
+      text: "#yunicode"
+    },
+    {
+      href: "https://twitter.com/hashtag/yunigraphics",
+      label: "FanArt", 
+      text: "#yunigraphics"
+    }
+  ];
+
+  return <LinkGroup links={links} />;
 }
