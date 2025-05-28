6b7412216f03e6c2065c4617ae09047a2df864cd
yuniruyuni <yuniruyuni@gmail.com>
2025-05-29 02:33:30 +0900

    refactor: split App.tsx into sub-components

---
+++ src/components/FanArtGuidelines.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function FanArtGuidelines() {
+  return (
+    <div className="bg-white bg-opacity-90 rounded-xl shadow-2xl p-8 max-w-4xl mx-auto my-16">
+      <header className="text-center mb-8">
+        <h2 className="text-2xl font-bold text-gray-600 mb-2">📚二次創作・ファンアートについて</h2>
+      </header>
+
+      <ul className="md:w-full text-center md:text-left list-outside list-disc">
+        <li className="text-gray-600">私のアバターはHoneycrisp様の<a href="https://booth.pm/ja/items/2198694" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">ユキちゃん</a>です。</li>
+        <li className="text-gray-600">そのため私は一次創作者ではないのです…<span className="font-bold">が、</span></li>
+        <li className="text-gray-600">Honeycrisp様に私(ゆにるユニ)の二次創作を誰でもやってよいという許可をいただいています✨</li>
+        <li className="text-gray-600 font-bold">そのためイラスト化やマンガ化といった二次創作、大歓迎です！</li>
+        <li className="text-gray-600">ただし他のユキちゃんとの区別として髪の色を水色にするか、胸元に時計を持たせてください。</li>
+        <li className="text-gray-600">またR18指定が必要なものについては、NSFW表示など未成年の視聴者への配慮を各々でお願いいたします。</li>
+        <li className="text-gray-600">宜しければ配信のサムネイルや SNS投稿に使用する可能性があることを了承の上 <a href="https://x.com/hashtag/yunigraphics" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">#yunigraphics</a> のタグ付きで投稿していただけると嬉しいです。</li>
+        <li className="text-gray-600 font-bold">この件については、ご迷惑になるのでHoneycrisp様に問い合わせるのはやめてください。</li>
+      </ul>
+    </div>
+  );
+}
